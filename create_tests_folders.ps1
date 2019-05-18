689fea02864e8f50cf1d2c5c411c27b14203b383
Morgan Pizzini <morgan.pizzini@hotmail.com>
2019-05-18 15:51:51 +0200

    create test folder script

---
+++ create_tests_folders.ps1
@@ -0,0 +1,21 @@
+﻿# remove folder if exists
+if(Test-Path -Path .\test) {
+ Remove-Item test -recurse
+}
+mkdir test
+cd test
+# 1: all opt dir
+mkdir sample1/node_modules
+mkdir sample1/obj
+mkdir sample1/bin
+# 2: node dir
+mkdir sample2/.vscode
+mkdir sample2/node_modules
+# 3: vs dir
+mkdir sample3/.vs
+mkdir sample3/obj
+mkdir sample3/bin
+# 4 tmp files dir
+mkdir sample4/.vs
+mkdir sample4/.vscode
+cd ..
